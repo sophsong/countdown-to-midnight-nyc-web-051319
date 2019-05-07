99402a3385bd7afff6330968d116d8bd326e387d
Sophia Song <ososanne@gmail.com>
2019-05-07 23:23:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
-
 def countdown
+  
+
+
+def countdown_with_sleep(num)
+  program= 1
+  while num >0 
+  puts "#{num} SECOND(S)!"
+  num-=1
+  end 
+"HAPPY NEW YEAR!"
 end
+
+
+
+
+
+# code method (countdown_with_sleep) to execute slower than 5 seconds
+# measure how long it takes to execute 
